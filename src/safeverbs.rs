e8b0649385e09004a752ded8156a8aead58152de
Jingrong Chen <crazyboycjr@gmail.com>
2024-11-17 23:47:01 -0500

    implement init to rtr, implemented a macro_rules to do that

--- src/safeverbs.rs
+++ src/safeverbs.rs
@@ -1,4 +1,5 @@
 #![allow(non_camel_case_types)]
+#![allow(clippy::too_many_arguments)]
 
 use crate::{ffi, ibv};
 use core::any::TypeId;
@@ -100,7 +101,7 @@ impl ProtectionDomain {
 pub struct QpType(pub ffi::ibv_qp_type::Type);
 
 pub trait ToQpType: 'static {
-    fn as_qp_type() -> QpType;
+    fn to_qp_type() -> QpType;
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -110,18 +111,23 @@ pub struct UC;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct UD;
 
+#[inline]
+fn is<S: ToQpType, T: ToQpType>() -> bool {
+    TypeId::of::<S>() == TypeId::of::<T>()
+}
+
 impl ToQpType for RC {
-    fn as_qp_type() -> QpType {
+    fn to_qp_type() -> QpType {
         QpType(ffi::ibv_qp_type::IBV_QPT_RC)
     }
 }
 impl ToQpType for UC {
-    fn as_qp_type() -> QpType {
+    fn to_qp_type() -> QpType {
         QpType(ffi::ibv_qp_type::IBV_QPT_UC)
     }
 }
 impl ToQpType for UD {
-    fn as_qp_type() -> QpType {
+    fn to_qp_type() -> QpType {
         QpType(ffi::ibv_qp_type::IBV_QPT_UD)
     }
 }
@@ -161,7 +167,7 @@ impl<T: ToQpType> QpInitAttr<T> {
                 .map_or(ptr::null_mut(), |cq| cq.cq.cq()),
             srq: ptr::null_mut(),
             cap: self.cap.0,
-            qp_type: T::as_qp_type().0,
+            qp_type: T::to_qp_type().0,
             sq_sig_all: self.sq_sig_all as i32,
         }
     }
@@ -267,6 +273,22 @@ pub struct QueuePair<T: ToQpType, S: ToQpState> {
 }
 
 impl<T: ToQpType, S: ToQpState> QueuePair<T, S> {
+    /// Bioperlate code to `ibv_modify_qp`.
+    unsafe fn modify<D: ToQpState>(
+        self,
+        attr: *mut ffi::ibv_qp_attr,
+        mask: ffi::ibv_qp_attr_mask,
+    ) -> io::Result<QueuePair<T, D>> {
+        let errno = unsafe { ffi::ibv_modify_qp(self.qp.qp, attr, mask.0 as i32) };
+        if errno != 0 {
+            return Err(io::Error::from_raw_os_error(errno));
+        }
+        Ok(QueuePair {
+            qp: self.qp,
+            _marker: PhantomData,
+        })
+    }
+
     pub fn modify_to_reset(self) -> io::Result<QueuePair<T, RESET>> {
         let mut attr = ffi::ibv_qp_attr {
             qp_state: ffi::ibv_qp_state::IBV_QPS_RESET,
@@ -275,14 +297,7 @@ impl<T: ToQpType, S: ToQpState> QueuePair<T, S> {
         let mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE;
         // SAFETY: FFI call. The FFI object obtained from a valid QueuePair object is
         // guaranteed to be valid.
-        let errno = unsafe { ffi::ibv_modify_qp(self.qp.qp, &mut attr, mask.0 as i32) };
-        if errno != 0 {
-            return Err(io::Error::from_raw_os_error(errno));
-        }
-        Ok(QueuePair {
-            qp: self.qp,
-            _marker: PhantomData,
-        })
+        unsafe { self.modify(&mut attr, mask) }
     }
 }
 
@@ -303,26 +318,158 @@ fn modify_reset_to_init<T: ToQpType>(
         | ffi::ibv_qp_attr_mask::IBV_QP_PKEY_INDEX
         | ffi::ibv_qp_attr_mask::IBV_QP_PORT;
 
-    if TypeId::of::<T>() == TypeId::of::<RC>() || TypeId::of::<T>() == TypeId::of::<UC>() {
+    if is::<T, RC>() || is::<T, UC>() {
         attr.qp_access_flags = access.expect("RC or UC must set ACCESS_FLAGS").0;
         mask |= ffi::ibv_qp_attr_mask::IBV_QP_ACCESS_FLAGS;
     }
-    if TypeId::of::<T>() == TypeId::of::<UD>() {
+    if is::<T, UD>() {
         attr.qkey = qkey.expect("UD must set qkey");
         mask |= ffi::ibv_qp_attr_mask::IBV_QP_QKEY;
     }
     // SAFETY: FFI call. The FFI object obtained from a valid QueuePair object is
     // guaranteed to be valid.
-    let errno = unsafe { ffi::ibv_modify_qp(qp.qp.qp, &mut attr, mask.0 as i32) };
-    if errno != 0 {
-        return Err(io::Error::from_raw_os_error(errno));
-    }
-    Ok(QueuePair {
-        qp: qp.qp,
-        _marker: PhantomData,
-    })
+    unsafe { qp.modify(&mut attr, mask) }
 }
 
+fn modify_init_to_init<T: ToQpType>(
+    qp: QueuePair<T, INIT>,
+    pkey_index: Option<u16>,
+    port_num: Option<u8>,
+    access: Option<ffi::ibv_access_flags>,
+    qkey: Option<u32>,
+) -> io::Result<QueuePair<T, INIT>> {
+    let mut attr = ffi::ibv_qp_attr {
+        qp_state: ffi::ibv_qp_state::IBV_QPS_INIT,
+        ..Default::default()
+    };
+    let mut mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE;
+
+    if let Some(pkey_index) = pkey_index {
+        attr.pkey_index = pkey_index;
+        mask |= ffi::ibv_qp_attr_mask::IBV_QP_PKEY_INDEX;
+    }
+    if let Some(port_num) = port_num {
+        attr.port_num = port_num;
+        mask |= ffi::ibv_qp_attr_mask::IBV_QP_PORT;
+    }
+    if is::<T, RC>() || is::<T, UC>() {
+        if let Some(access) = access {
+            attr.qp_access_flags = access.0;
+            mask |= ffi::ibv_qp_attr_mask::IBV_QP_ACCESS_FLAGS;
+        }
+    }
+    if is::<T, UD>() {
+        if let Some(qkey) = qkey {
+            attr.qkey = qkey;
+            mask |= ffi::ibv_qp_attr_mask::IBV_QP_QKEY;
+        }
+    }
+    // SAFETY: FFI call. The FFI object obtained from a valid QueuePair object is
+    // guaranteed to be valid.
+    unsafe { qp.modify(&mut attr, mask) }
+}
+
+macro_rules! impl_modify_qp {
+    ($func_name:ident, $qp_state_begin:ident, $qp_state_end:ident,
+        $(required: ($($qpt:ident),+ $(,)?) [$mask_tag:ident] $field_name:ident: $field_type:ty,)*
+        $(optional: ($($qpt_opt:ident),+ $(,)?) [$mask_tag_opt:ident] $field_name_opt:ident: $field_type_opt:ty,)*
+        $(alt_path: ($($qpt_alt_path:ident),+ $(,)?) [$mask_tag_alt_path:ident] $field_name_alt_path:ident: $field_type_alt_path:ty,)?
+    ) => {
+        fn $func_name<T: ToQpType>(
+            qp: QueuePair<T, $qp_state_begin>,
+            $($field_name: $field_type,)*
+            $($field_name_opt: $field_type_opt,)*
+            $($field_name_alt_path: $field_type_alt_path,)?
+        ) -> io::Result<QueuePair<T, $qp_state_end>> {
+            let mut attr = ffi::ibv_qp_attr {
+                qp_state: ffi::ibv_qp_state::IBV_QPS_RTR,
+                ..Default::default()
+            };
+            let mut mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE;
+            // build required fields
+            $(
+            // for each qp type in [RC, UC, UD]
+            $(
+            if is::<T, $qpt>() {
+                attr.$field_name = $field_name.expect(concat!(stringify!($field_name), " is required"));
+                mask |= ffi::ibv_qp_attr_mask::$mask_tag;
+            })*
+            )*
+            // build optional fields
+            $(
+            // for each qp type in [RC, UC, UD]
+            $(
+            if is::<T, $qpt_opt>() {
+                if let Some($field_name_opt) = $field_name_opt {
+                    attr.$field_name_opt = $field_name_opt;
+                    mask |= ffi::ibv_qp_attr_mask::$mask_tag_opt;
+                }
+            })*
+            )*
+            // build fields for alternative path
+            $(
+            // for each qp type in [RC, UC, UD]
+            $(
+            if is::<T, $qpt_alt_path>() {
+                if let Some(alt_path) = $field_name_alt_path {
+                    attr.alt_ah_attr = alt_path.alt_ah_attr;
+                    attr.alt_pkey_index = alt_path.alt_pkey_index;
+                    attr.alt_port_num = alt_path.alt_port_num;
+                    attr.alt_timeout = alt_path.alt_timeout;
+                    mask |= ffi::ibv_qp_attr_mask::$mask_tag_alt_path;
+                }
+            }
+            )*
+            )?
+            // SAFETY: FFI call. The FFI object obtained from a valid QueuePair object is
+            // guaranteed to be valid.
+            unsafe { qp.modify(&mut attr, mask) }
+        }
+    };
+}
+
+impl_modify_qp!(modify_init_to_rtr, INIT, RTR,
+    required: (RC, UC) [IBV_QP_AV] ah_attr: Option<ffi::ibv_ah_attr>,
+    required: (RC, UC) [IBV_QP_PATH_MTU] path_mtu: Option<u32>,
+    required: (RC, UC) [IBV_QP_DEST_QPN] dest_qp_num: Option<u32>,
+    required: (RC, UC) [IBV_QP_RQ_PSN] rq_psn: Option<u32>,
+    required: (RC) [IBV_QP_MAX_DEST_RD_ATOMIC] max_dest_rd_atomic: Option<u8>,
+    required: (RC) [IBV_QP_MIN_RNR_TIMER] min_rnr_timer: Option<u8>,
+    optional: (RC, UC, UD) [IBV_QP_PKEY_INDEX] pkey_index: Option<u16>,
+    optional: (RC, UC) [IBV_QP_ACCESS_FLAGS] qp_access_flags: Option<u32>,
+    optional: (UD) [IBV_QP_QKEY] qkey: Option<u32>,
+    alt_path: (RC, UC) [IBV_QP_ALT_PATH] alt_path: Option<AltPath>,
+);
+
+// fn modify_init_to_rtr2<T: ToQpType>(
+//     qp: QueuePair<T, INIT>,
+//     ah_attr: ffi::ibv_ah_attr,
+//     path_mtu: u32,
+//     dest_qpn: u32,
+//     rq_psn: u32,
+//     max_dest_rd_atomic: Option<u8>,
+//     min_rnr_timer: Option<u8>,
+//     pkey_index: Option<u16>,
+//     access: Option<ffi::ibv_access_flags>,
+//     alt_path: Option<AltPath>,
+// ) -> io::Result<QueuePair<T, RTR>> {
+//     let mut attr = ffi::ibv_qp_attr {
+//         qp_state: ffi::ibv_qp_state::IBV_QPS_RTR,
+//         ..Default::default()
+//     };
+//     let mut mask = ffi::ibv_qp_attr_mask::IBV_QP_STATE;
+//     if is::<T, RC>() {
+//         if let Some(max_dest_rd_atomic) = max_dest_rd_atomic {
+//             attr.max_dest_rd_atomic = max_dest_rd_atomic;
+//             mask |= ffi::ibv_qp_attr_mask::IBV_QP_MAX_DEST_RD_ATOMIC;
+//         }
+//     }
+//     // SAFETY: FFI call. The FFI object obtained from a valid QueuePair object is
+//     // guaranteed to be valid.
+//     unsafe { qp.modify(&mut attr, mask) }
+// }
+
+// RESET -> INIT
 impl QueuePair<UC, RESET> {
     pub fn modify_to_init(
         self,
@@ -353,5 +500,116 @@ impl QueuePair<UD, RESET> {
         qkey: u32,
     ) -> io::Result<QueuePair<UD, INIT>> {
         modify_reset_to_init(self, pkey_index, port_num, None, Some(qkey))
+    }
+}
+
+// INIT -> INIT
+impl QueuePair<UC, INIT> {
+    pub fn modify_to_init(
+        self,
+        pkey_index: Option<u16>,
+        port_num: Option<u8>,
+        access: Option<ffi::ibv_access_flags>,
+    ) -> io::Result<QueuePair<UC, INIT>> {
+        modify_init_to_init(self, pkey_index, port_num, access, None)
+    }
+}
+
+impl QueuePair<RC, INIT> {
+    pub fn modify_to_init(
+        self,
+        pkey_index: Option<u16>,
+        port_num: Option<u8>,
+        access: Option<ffi::ibv_access_flags>,
+    ) -> io::Result<QueuePair<RC, INIT>> {
+        modify_init_to_init(self, pkey_index, port_num, access, None)
+    }
+}
+
+impl QueuePair<UD, INIT> {
+    pub fn modify_to_init(
+        self,
+        pkey_index: Option<u16>,
+        port_num: Option<u8>,
+        qkey: Option<u32>,
+    ) -> io::Result<QueuePair<UD, INIT>> {
+        modify_init_to_init(self, pkey_index, port_num, None, qkey)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct AltPath {
+    alt_ah_attr: ffi::ibv_ah_attr,
+    alt_pkey_index: u16,
+    alt_port_num: u8,
+    alt_timeout: u8,
+}
+
+// INIT -> RTR
+impl QueuePair<UC, INIT> {
+    pub fn modify_to_rtr(
+        self,
+        ah_attr: ffi::ibv_ah_attr,
+        path_mtu: u32,
+        dest_qpn: u32,
+        rq_psn: u32,
+        pkey_index: Option<u16>,
+        access: Option<ffi::ibv_access_flags>,
+        alt_path: Option<AltPath>,
+    ) -> io::Result<QueuePair<UC, RTR>> {
+        modify_init_to_rtr(
+            self,
+            Some(ah_attr),
+            Some(path_mtu),
+            Some(dest_qpn),
+            Some(rq_psn),
+            None, // max_dest_rd_atomic
+            None, // min_rnr_timer
+            pkey_index,
+            access.map(|x| x.0),
+            None, // qkey (optional fields for UD only)
+            alt_path,
+        )
+    }
+}
+
+impl QueuePair<RC, INIT> {
+    pub fn modify_to_rtr(
+        self,
+        ah_attr: ffi::ibv_ah_attr,
+        path_mtu: u32,
+        dest_qpn: u32,
+        rq_psn: u32,
+        max_dest_rd_atomic: u8,
+        min_rnr_timer: u8,
+        pkey_index: Option<u16>,
+        access: Option<ffi::ibv_access_flags>,
+        alt_path: Option<AltPath>,
+    ) -> io::Result<QueuePair<RC, RTR>> {
+        modify_init_to_rtr(
+            self,
+            Some(ah_attr),
+            Some(path_mtu),
+            Some(dest_qpn),
+            Some(rq_psn),
+            Some(max_dest_rd_atomic),
+            Some(min_rnr_timer),
+            pkey_index,
+            access.map(|x| x.0),
+            None, // qkey (optional fields for UD only)
+            alt_path,
+        )
+    }
+}
+
+impl QueuePair<UD, INIT> {
+    pub fn modify_to_rtr(
+        self,
+        pkey_index: Option<u16>,
+        qkey: Option<u32>,
+    ) -> io::Result<QueuePair<UD, RTR>> {
+        modify_init_to_rtr(
+            self, None, None, None, None, None, None, pkey_index, None, qkey, None,
+        )
     }
 }
